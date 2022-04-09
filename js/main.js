b87aee9b99fa6db52d222c613b99ae1aac33e2a4
WebJubayergit config --global user.email <you@example.com>
2022-04-09 21:28:51 +0600

    frontend done

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+const totalExpense = foodCost + rentCost + clotheCost;
+
+
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    console.log('calllu')
+    const foodCost = document.getElementById('food-cost');
+    const foodCostAmount = foodCost.value;
+    console.log(foodCostAmount);
+
+
+
+    
+  
+
+    
+})
